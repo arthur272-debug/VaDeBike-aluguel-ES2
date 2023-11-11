a636e13e6d6b7bcb250cce7ead1aee0312ff37cb
Arthur André <arthurdasilvaandre@gmail.com>
2023-11-11 08:59:37 -0300

    update comentário

---
+++ app/models/Funcionario.py
@@ -0,0 +1,12 @@
+class Funcionario:
+    
+    def __init__(self,senha,cpf,email,documento,funcao,idade,nome):
+        self.senha= senha
+        self.cpf= cpf
+        self.email= email
+        self.documento= documento
+        self.funcao= funcao
+        self.idade = idade
+        self.nome = nome
+    
+    
