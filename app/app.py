00573ae2a610ab062d3eeccb494fbce3d585b794
Arthur André <arthurdasilvaandre@gmail.com>
2023-11-11 09:11:19 -0300

    add comentarios

--- app/app.py
+++ app/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, jsonify, request
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
 #mudar o código
-@app.route('/')
+#@app.route('/')
 
-def index():
-    return '<h1>Hello, World!</h1>', 200
-if __name__ == '_main_':
- app.run(port=8080,host='localhost',debug=false)
+#def index():
+#    return '<h1>Hello, World!</h1>', 200
+#if __name__ == '_main_':
+# app.run(port=8080,host='localhost',debug=false)
