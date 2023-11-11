d80ea1fb83cadeede905cddd798d61db813eebdc
Arthur André <arthurdasilvaandre@gmail.com>
2023-11-11 09:10:49 -0300

    update funcionarioDTO e funcionario

---
+++ app/dto/FuncionarioDTO.py
@@ -0,0 +1,10 @@
+class FuncionarioDto:
+    
+    def __init__(self,senha,cpf,email,documento,funcao,idade,nome):
+        self.senha= senha
+        self.cpf= cpf
+        self.email= email
+        self.documento= documento
+        self.funcao= funcao
+        self.idade = idade
+        self.nome = nome
