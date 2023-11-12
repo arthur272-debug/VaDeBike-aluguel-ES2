fef5fca89bf7b486d0c9634f78ef19eff0d314fc
Arthur André <arthurdasilvaandre@gmail.com>
2023-11-12 16:08:05 -0300

    consertando erros

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
 from flask import Flask
-#from controllers import 
+from controllers.FuncionarioController import funcionarioBp
 #from controllers import 
 
 app = Flask(__name__)
+app.register_blueprint(funcionarioBp)
 
-if __name__ == '_main_':
-app.run(host = '0.0.0.0', debug=True, port=5000)
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', debug=True, port=5000)
