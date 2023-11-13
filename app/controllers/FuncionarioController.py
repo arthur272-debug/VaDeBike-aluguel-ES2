8b887998d3bfd96b7dc0769e4bf6e915b41ad894
Arthur André <arthurdasilvaandre@gmail.com>
2023-11-13 10:03:13 -0300

    Update FuncionarioController.py

--- app/controllers/FuncionarioController.py
+++ app/controllers/FuncionarioController.py
@@ -8,27 +8,27 @@ funcionarioBp = Blueprint('funcionarioBp',__name__)
 def realizarCadastro(): # colocar a parte da exceção
     funcionarioDados= request.json
     funcionarioDto= FuncionarioDTO(funcionarioDados['senha'],funcionarioDados['cpf'], funcionarioDados['email'],funcionarioDados['documento'],funcionarioDados['funcao'],funcionarioDados['idade'],funcionarioDados['nome'])
-    funcionario = FuncionarioService.cadastrarFuncionario(funcionarioDto)
+    funcionario = FuncionarioService.FuncionarioService.cadastrarFuncionario(funcionarioDto)
     return jsonify(funcionario.__dict__), 200
 
 @funcionarioBp.route('/funcionario',methods=['GET'])
 def realizarListagenFuncionarios():# colocar a parte da exceção
-    funcionarios= FuncionarioService.consultarListaFuncionario()
-    funcionariosDto= [FuncionarioDTO(funcionario.senha, funcionario.cpf, funcionario.email, funcionario.documento, funcionario.funcao,funcionario.idade,funcionario.nome) for funcionario in funcionarios]
+    funcionarios= FuncionarioService.FuncionarioService.funcionarios
+    funcionariosDto= [FuncionarioDTO.FuncionarioDto(funcionario.senha, funcionario.cpf, funcionario.email, funcionario.documento, funcionario.funcao,funcionario.idade,funcionario.nome) for funcionario in funcionarios]
     return jsonify([funcionario.__dict__ for funcionario in funcionariosDto]),200
 
 @funcionarioBp.route('/funcionario/<int:funcionarioId>',methods=['GET'])
 def realizarBuscaFuncionario(funcionarioId):# colocar a parte da exceção
-    funcionario =FuncionarioService.consultarFuncionario(funcionarioId)
+    funcionario =FuncionarioService.FuncionarioService.consultarFuncionario(funcionarioId)
     if funcionario is not None:
-        funcionarioDto= FuncionarioDTO(funcionario.senha,funcionario.cpf, funcionario.email, funcionario.documento, funcionario.funcao,funcionario.idade,funcionario.nome)
+        funcionarioDto= FuncionarioDTO.FuncionarioDto(funcionario.senha,funcionario.cpf, funcionario.email, funcionario.documento, funcionario.funcao,funcionario.idade,funcionario.nome)
         return jsonify(funcionarioDto.__dict__),200
     else:
         return "Funcionário não encontrado",404
     
 @funcionarioBp.route('/funcionario/<int:funcionarioId>',methods=['PUT'])
 def realizarAtualizacaoFuncionario(funcionarioId):# colocar a parte da exceção
-        funcionario = FuncionarioService.consultarFuncionario(funcionarioId)
+        funcionario = FuncionarioService.FuncionarioService.consultarFuncionario(funcionarioId)
         if funcionario is not None:
             funcionarioData = request.json
             funcionario.senha= funcionarioData.get('senha',funcionario.senha)
@@ -45,10 +45,10 @@ def realizarAtualizacaoFuncionario(funcionarioId):# colocar a parte da exceção
         
 @funcionarioBp.route('/funcionario/<int:funcionarioId>',methods=['DELETE'])
 def realizarExclusaoFuncionario(funcionarioId):# colocar a parte da exceção
-     funcionario = FuncionarioService.consultarFuncionario(funcionarioId)
+     funcionario = FuncionarioService.FuncionarioService.consultarFuncionario(funcionarioId)
 
      if funcionario is not None:
-          FuncionarioService.deletarFuncionario(funcionarioId)
+          FuncionarioService.FuncionarioService.deletarFuncionario(funcionarioId)
           return "Funcionário excluído com sucesso",200
      else:
           return "Funcionário não encontrado",404
