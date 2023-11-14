add3779cd7b65cb51b72cdfca52a1fe3ca6af7b2
Arthur André <arthurdasilvaandre@gmail.com>
2023-11-14 17:09:16 -0300

    Update CiclistaDTO.py

--- app/dto/CiclistaDTO.py
+++ app/dto/CiclistaDTO.py
@@ -1,11 +1,12 @@
+from models import Nacionalidade
+
 class CiclistaDto:
 
-    def __init__(self,nome:str,nascimento:str,cpf:str,passaporte:object,nacionalidade:str,email:str,urlFotoDocumento:str,id:int):
+    def __init__(self,nome:str,nascimento:str,cpf:str,passaporte:object,nacionalidade,email:str,urlFotoDocumento:str,id:int):
         self.nome= nome
         self.nascimento = nascimento
         self.cpf = cpf
         self.passaporte = passaporte
-        self.nacionalidade = nacionalidade
         self.email = email
         self.urlFotoDocumento = urlFotoDocumento
         self.id = id
