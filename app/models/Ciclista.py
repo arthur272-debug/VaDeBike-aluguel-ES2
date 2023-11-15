c04fa2ed0d28b9308277bc8acc1493e37d1811d0
Arthur André <arthurdasilvaandre@gmail.com>
2023-11-15 09:46:53 -0300

    alteracoes nas classes

--- app/models/Ciclista.py
+++ app/models/Ciclista.py
@@ -5,9 +5,15 @@ class Nacionalidade(Enum):
     BRASILEIRA= "BRASILEIRA"
     ESTRANGEIRA= "ESTRANGEIRA"
 
+class RespostaCadastro(Enum):
+    
+    ATIVO = "ATIVO"
+    INATIVO = "INATIVO"
+    CONFIRMACAO = "AGUARDANDO_CONFIRMACAO"
+
 class Ciclista:
 
-    def __init__(self,nome:str,nascimento:str,cpf:str,passaporte:object,nacionalidade,email:str,urlFotoDocumento:str,id:int,senha:str):
+    def __init__(self,nome:str,nascimento:str,cpf:str,passaporte:object,nacionalidade,email:str,urlFotoDocumento:str,id:int,senha:str,cadastro):
         self.nome= nome
         self.nascimento = nascimento
         self.cpf = cpf
