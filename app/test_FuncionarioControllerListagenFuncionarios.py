f78678becd68ccf46f9486031ba5e724fc87dc59
Arthur André <arthurdasilvaandre@gmail.com>
2023-11-20 15:21:40 -0300

    substituindo pelo mock

--- app/test_FuncionarioControllerListagenFuncionarios.py
+++ app/test_FuncionarioControllerListagenFuncionarios.py
@@ -1,12 +1,11 @@
 import unittest
-from flask import Flask, jsonify
+from flask import Flask
 from services import FuncionarioService
 from controllers.FuncionarioController import funcionarioBp
-from dto import FuncionarioDTO
+from unittest.mock import MagicMock
 
 class TestFuncionarioRouteListagem(unittest.TestCase):
     def setUp(self):
-        
         # Configuração inicial para os testes
         self.app = Flask(__name__)
         self.app.register_blueprint(funcionarioBp)
@@ -16,32 +15,33 @@ class TestFuncionarioRouteListagem(unittest.TestCase):
     def test_realizar_listagem_funcionarios_sucesso(self):
         # Configurar dados fictícios para um teste bem-sucedido
 
-         funcionario =  FuncionarioDTO.FuncionarioDto("12345678910","12345678910123","arthur.tutu@gmail.com","rg","Analista de Jogos",25,"Tutu",1)
-         funcionario2 = FuncionarioDTO.FuncionarioDto("1234567800","12345678910","arthur.andre@gmail.com","cpf","Analista de T.I.",21,"Arthur",2)
+        
+        funcionario_mock1 = MagicMock(senha="1234",cpf="123456789",email="arthur.andre@gmail.com",documento="cpf",funcao="analista de segurança",idade = 21,nome="Nome1",id=1)
+        funcionario_mock2 = MagicMock(senha="123456",cpf="12345678910",email="arthur.andre@gmailfdfdf.com",documento="rg",funcao="analista de T.I,",idade = 27,nome="Nome3",id=2)
 
-         funcionarios_mock = [funcionario,funcionario2]
-    
+        funcionarios_mock = [funcionario_mock1,funcionario_mock2]
+
         # Sobrescrever o método de consulta para retornar dados fictícios
-         FuncionarioService.FuncionarioService.consultar_lista_funcionario = lambda: funcionarios_mock
+        FuncionarioService.FuncionarioService.consultar_lista_funcionario = lambda: funcionarios_mock
 
         # Chame a rota /funcionario com o método GET
-         resposta = self.client.get('/funcionario')
+        resposta = self.client.get('/funcionario')
 
         # Verifique se a resposta é 200 OK e se contém os dados esperados
-         self.assertEqual(resposta.status_code, 200)
-         dados_resposta = resposta.get_json()
-         self.assertIsInstance(dados_resposta, list)
-         self.assertEqual(len(dados_resposta), len(funcionarios_mock))
-        
+        self.assertEqual(resposta.status_code, 200)
+        dados_resposta = resposta.get_json()
+        self.assertIsInstance(dados_resposta, list)
+        self.assertEqual(len(dados_resposta), len(funcionarios_mock))
 
     def test_realizar_listagem_funcionarios_nao_encontrado(self):
-
         # Sobrescrever o método de consulta para retornar uma lista vazia
         FuncionarioService.FuncionarioService.consultar_lista_funcionario = lambda: []
+
         # Chame a rota /funcionario com o método GET
         resposta = self.client.get('/funcionario')
 
         # Verifique se a resposta é 404 Not Found
         self.assertEqual(resposta.status_code, 404)
+
 if __name__ == '__main__':
     unittest.main()
