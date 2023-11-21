dabffc34079c054fea2ea41a433282aaf61b0235
Arthur André <arthurdasilvaandre@gmail.com>
2023-11-21 09:11:29 -0300

    add id nas classes cartao e cartaoDTO

--- app/models/Cartao.py
+++ app/models/Cartao.py
@@ -1,7 +1,8 @@
 class cartao:
 
-    def __init__(self,nomeTitular:str,numero:str,validade:str,cvv:str):
+    def __init__(self,nomeTitular:str,numero:str,validade:str,cvv:str,id:int):
         self.nomeTitular=nomeTitular
         self.numero=numero
         self.validade=validade
         self.cvv=cvv
+        self.id=id
