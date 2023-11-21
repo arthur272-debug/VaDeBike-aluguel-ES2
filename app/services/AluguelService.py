c1b19e37fefd898ad50eef2478c5f2f988b05f13
Arthur André <arthurdasilvaandre@gmail.com>
2023-11-21 16:54:58 -0300

    Update AluguelService.py

---
+++ app/services/AluguelService.py
@@ -0,0 +1,60 @@
+from services import CiclistaService
+
+lista_ciclista = CiclistaService.CiclistaService.Ciclista
+
+alugueis_historico = []
+
+class AluguelService:
+
+# usado também para ver se o ciclista tem bicicleta alugada ou não
+    @staticmethod
+    def verificar_ciclista_aluguel(id_ciclista): 
+        ciclista = None
+        aluguel = None
+        for ciclista in lista_ciclista:
+            if ciclista.id == id_ciclista:
+                aluguel = ciclista.aluguel
+                break
+        return aluguel
+    
+    @staticmethod
+    def alugar_bicicleta(id_ciclista,aluguel_dto): #integração aqui
+       ciclista = None
+       aluguel = None
+       for ciclista in lista_ciclista:
+           if ciclista.id == id_ciclista:
+              aluguel = ciclista.aluguel
+              break
+        
+       if aluguel is None:
+          return aluguel
+       aluguel.ciclista = id_ciclista
+       aluguel.trancaInicio = aluguel_dto.trancaInicio
+       aluguel.bicicleta = len(alugueis_historico)+1 # muda -- fake
+       aluguel.horaInicio = aluguel_dto.horaInicio
+      
+       return aluguel
+    
+    @staticmethod #verificar aqui
+    def devolver_bicicleta(id_ciclista,id_bicicleta,id_tranca): #integração aqui
+       ciclista = None
+       id_tranca = 0 # será alterado pela integração
+       aluguel = None
+       for ciclista in lista_ciclista:
+           if ciclista.id == id_ciclista:
+              aluguel = ciclista.aluguel
+              break
+        
+       if aluguel is None:
+          return aluguel
+       
+       # pega as informações dos outros microsserviços
+       aluguel.trancaFim =0 
+       aluguel.horaFim = 0
+       aluguel.cobranca = 0
+       
+       # informações do aluguel e pagamento serão repassadas para seus microsserviços responsáveis
+       alugueis_historico.append(aluguel)
+       ciclista.aluguel = None
+       return aluguel
+    
