cb9342df7bd98923fcfb54080a410f775bd69d22
Arthur André <arthurdasilvaandre@gmail.com>
2023-11-21 20:21:12 -0300

    criacao de classe fake

---
+++ app/models/Bicicleta_fake.py
@@ -0,0 +1,9 @@
+class bicicleta:  # será substituída pela integração
+
+    def __init__(self,marca:str,modelo:str,ano:str,numero:int,status:str):
+        
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+        self.numero = numero
+        self.status = status
