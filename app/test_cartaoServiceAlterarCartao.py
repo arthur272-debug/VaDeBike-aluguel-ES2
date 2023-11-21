d6e97a0536292ecaed5d98d91d7382a7f2f67dcc
Arthur André <arthurdasilvaandre@gmail.com>
2023-11-21 13:31:23 -0300

    tirando importacao desnecessaria

--- app/test_cartaoServiceAlterarCartao.py
+++ app/test_cartaoServiceAlterarCartao.py
@@ -1,5 +1,5 @@
 import unittest
-from services import CartaoService,CiclistaService
+from services import CartaoService
 from unittest.mock import MagicMock
 
 class TestCartaoServiceAlterar(unittest.TestCase):
@@ -8,7 +8,7 @@ class TestCartaoServiceAlterar(unittest.TestCase):
         # Configuração inicial para os testes
         cartao_mock = MagicMock(nomeTitular="Titular Antigo", numero="1111222233334444", validade="12/25", cvv="123", id=1)
         ciclista_mock1 = MagicMock(nome="Arthur", nascimento="27/03/2002", cpf="122323", passaporte=None,nacionalidade="Brasileiro", email="arthur.andre@gmail.com", urlFotoDocumento="httpssssss",id=1, senha="12356", cadastro=None, cartao=cartao_mock, aluguel=None)
-        CiclistaService.CiclistaService.Ciclista.append(ciclista_mock1)
+        CartaoService.ciclista_lista.append(ciclista_mock1)
 
     def test_alterar_cartao_sucesso(self):
         # Configurar dados fictícios para um teste bem-sucedido
