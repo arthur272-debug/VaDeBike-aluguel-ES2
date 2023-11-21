db61fc26792d58a3f83657b4a61d616e2e8569cd
Arthur André <arthurdasilvaandre@gmail.com>
2023-11-20 21:20:08 -0300

    add informacoes cartao

---
+++ app/models/Cartao.py
@@ -0,0 +1,7 @@
+class cartao:
+
+    def __init__(self,nomeTitular:str,numero:str,validade:str,cvv:str):
+        self.nomeTitular=nomeTitular
+        self.numero=numero
+        self.validade=validade
+        self.cvv=cvv
