0bd448847a2530c2a205fe57242b6d6b5f8f5e18
Arthur André <arthurdasilvaandre@gmail.com>
2023-11-20 21:27:08 -0300

    add atributos aluguel

---
+++ app/dto/AluguelDTO.py
@@ -0,0 +1,10 @@
+class AluguelDto:
+
+    def __init__(self,ciclista:int,trancaInicio:int,bicicleta:int,horaInicio:int,trancaFim:int,horaFim:int,cobranca:int):
+        self.ciclista = ciclista
+        self.trancaInicio= trancaInicio
+        self.bicicleta = bicicleta
+        self.horaInicio = horaInicio
+        self.trancaFim = trancaFim
+        self.horaFim= horaFim
+        self.cobranca = cobranca
