775d4f98fd823058162d4a1ffc2d5f3e168d71d1
Arthur André <arthurdasilvaandre@gmail.com>
2023-11-22 19:23:08 -0300

    Update CartaoController.py

--- app/controllers/CartaoController.py
+++ app/controllers/CartaoController.py
@@ -8,18 +8,18 @@ invalido = "Dados Inválidos"
 @cartaoBp.route('/cartaoDeCredito/<int:id_ciclista>',methods=['PUT'])
 def realizar_atulizacao(id_ciclista):
     cartao_dados = request.json
-    if not '-' in cartao_dados["validade"]:
+    if '-' not in cartao_dados["validade"]:
         return invalido,422
 
-    cartao_Dto = CartaoDTO.CartaoDto(cartao_dados["nomeTitular"],cartao_dados["numero"],cartao_dados["validade"],cartao_dados["cvv"],id_ciclista)
-    cartao = CartaoService.CartaoService.alterar_cartao(id_ciclista,cartao_Dto)
+    cartao_dto = CartaoDTO.CartaoDto(cartao_dados["nomeTitular"],cartao_dados["numero"],cartao_dados["validade"],cartao_dados["cvv"],id_ciclista)
+    cartao = CartaoService.CartaoService.alterar_cartao(id_ciclista,cartao_dto)
     if cartao is not None:
         return "Dados atualizados",200
     else:
         return "Não encontrado",404
     
 @cartaoBp.route('/cartaoDeCredito/<int:id_ciclista>',methods=['GET'])
-def realizar_consultaCartao(id_ciclista):
+def realizar_consulta_cartao(id_ciclista):
     cartao= CartaoService.CartaoService.consultar_cartao(id_ciclista)
     if cartao is not None:
         informacoes_cartao = {"id":cartao.id,"nomeTitular":cartao.nomeTitular,"numero":cartao.numero,"validade":cartao.validade,"cvv":cartao.cvv}
