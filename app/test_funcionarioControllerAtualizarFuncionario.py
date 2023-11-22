a6165195372192f5bde53f41e1cd5a43fde12e78
Arthur André <arthurdasilvaandre@gmail.com>
2023-11-22 19:06:31 -0300

    Update test_funcionarioControllerAtualizarFuncionario.py

--- app/test_funcionarioControllerAtualizarFuncionario.py
+++ app/test_funcionarioControllerAtualizarFuncionario.py
@@ -9,6 +9,7 @@ class TestFuncionarioRouteAtualizacao(unittest.TestCase):
     def setUp(self):
         # Configuração inicial para os testes
         self.app = Flask(__name__)
+        self.app.config['WTF_CSRF_ENABLED'] = False
         self.app.register_blueprint(funcionarioBp)
         self.app.config['TESTING'] = True
         self.client = self.app.test_client()
