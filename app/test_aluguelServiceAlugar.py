acdf3df657dacb7355f67aefbf7a9dc864957775
Arthur André <arthurdasilvaandre@gmail.com>
2023-11-23 12:23:37 -0300

    Update test_aluguelServiceAlugar.py

--- app/test_aluguelServiceAlugar.py
+++ app/test_aluguelServiceAlugar.py
@@ -8,33 +8,12 @@ class TestAlugarBicicleta(unittest.TestCase):
     def setUp(self):
         # Configuração inicial para os testes
         self.contador = 1
-        ciclista = Ciclista.Ciclista(
-            nome="Arthur",
-            nascimento="27/03/2002",
-            cpf="122323",
-            passaporte=None,
-            nacionalidade="Brasileiro",
-            email="arthur.andre",
-            urlFotoDocumento="https",
-            id=self.contador,
-            senha="12356",
-            cadastro=None,
-            cartao=None,
-            aluguel=None
-        )
+        ciclista = Ciclista.Ciclista(nome="Arthur",nascimento="27/03/2002",cpf="122323",passaporte=None,nacionalidade="Brasileiro",email="arthur.andre",urlFotoDocumento="https",id=self.contador,senha="12356",cadastro=None,cartao=None,aluguel=None)
         AluguelService.lista_ciclista.append(ciclista)
 
     def test_alugar_bicicleta_ciclista_nao_encontrado(self):
         # Configurar dados fictícios para um teste de falha
-        aluguel_dto_invalido = MagicMock(
-            ciclista=1000,
-            trancaInicio=1234,
-            bicicleta=1,
-            horaInicio=0,
-            trancaFim=0,
-            horaFim=0,
-            cobranca=0
-        )
+        aluguel_dto_invalido = MagicMock(ciclista=1000,trancaInicio=1234,bicicleta=1,horaInicio=0,trancaFim=0,horaFim=0,cobranca=0)
 
         # Chame a função alugar_bicicleta
         resultado = AluguelService.AluguelService.alugar_bicicleta(aluguel_dto_invalido)
@@ -44,15 +23,7 @@ class TestAlugarBicicleta(unittest.TestCase):
 
     def test_alugar_bicicleta_sucesso(self):
          # Chame a função alugar_bicicleta
-        aluguel_dto_valido = MagicMock(
-            ciclista=self.contador,
-            trancaInicio=1234,
-            bicicleta=1,
-            horaInicio=0,
-            trancaFim=0,
-            horaFim=0,
-            cobranca=0
-        )
+        aluguel_dto_valido = MagicMock(ciclista=self.contador,trancaInicio=1234,bicicleta=1,horaInicio=0,trancaFim=0,horaFim=0,cobranca=0)
 
         resultado = AluguelService.AluguelService.alugar_bicicleta(aluguel_dto_valido)
 
@@ -67,15 +38,7 @@ class TestAlugarBicicleta(unittest.TestCase):
 
     def test_alugar_bicicleta_sucesso_alugada(self):
         # Chame a função alugar_bicicleta
-        aluguel_dto_valido = MagicMock(
-            ciclista=1,
-            trancaInicio=1234,
-            bicicleta=1,
-            horaInicio=0,
-            trancaFim=0,
-            horaFim=0,
-            cobranca=0
-        )
+        aluguel_dto_valido = MagicMock(ciclista=1,trancaInicio=1234,bicicleta=1,horaInicio=0,trancaFim=0,horaFim=0,cobranca=0)
 
         # Chame a função alugar_bicicleta
         resultado = AluguelService.AluguelService.alugar_bicicleta(aluguel_dto_valido)
