ed353ab59b2c91e3f0309e6dc2488f3b56f142df
Arthur André <arthurdasilvaandre@gmail.com>
2023-11-23 11:34:21 -0300

    Update test_aluguelServiceBicicletaAlugada.py

--- app/test_aluguelServiceBicicletaAlugada.py
+++ app/test_aluguelServiceBicicletaAlugada.py
@@ -13,6 +13,16 @@ class TestVerificarBicicletaAlugada(unittest.TestCase):
         Bicicleta_fake.bicicleta = MagicMock(return_value=MagicMock())
         Bicicleta_fake.bicicleta.bicicleta = MagicMock(return_value=MagicMock())
 
+    def test_verificar_bicicleta_alugada_sem_aluguel(self):
+        # Configurar dados fictícios para um teste de falha
+        id_ciclista_invalido = 999
+
+        # Chame a função verificar_bicicleta_alugada
+        resultado = AluguelService.AluguelService.verificar_bicicleta_alugada(id_ciclista_invalido)
+
+        # Verifique se o resultado é None (ciclista não encontrado)
+        self.assertFalse(resultado)
+
     def test_verificar_bicicleta_alugada_com_aluguel(self):
         # Configurar dados fictícios para um teste bem-sucedido
         id_ciclista = 1
@@ -23,15 +33,6 @@ class TestVerificarBicicletaAlugada(unittest.TestCase):
         # Verifique se o resultado é Não-nulo
         self.assertIsNotNone(resultado)
 
-    def test_verificar_bicicleta_alugada_sem_aluguel(self):
-        # Configurar dados fictícios para um teste de falha
-        id_ciclista_invalido = 999
-
-        # Chame a função verificar_bicicleta_alugada
-        resultado = AluguelService.AluguelService.verificar_bicicleta_alugada(id_ciclista_invalido)
-
-        # Verifique se o resultado é None (ciclista não encontrado)
-        self.assertFalse(resultado)
 
 if __name__ == '__main__':
     unittest.main()
