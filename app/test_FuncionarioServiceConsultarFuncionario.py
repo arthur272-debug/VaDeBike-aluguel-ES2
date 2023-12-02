0ec6d5e4a21ab9cf2319c14fe1f335d995a7aacd
Arthur André <arthurdasilvaandre@gmail.com>
2023-12-02 15:57:08 -0300

    Update test_FuncionarioServiceConsultarFuncionario.py

--- app/test_FuncionarioServiceConsultarFuncionario.py
+++ app/test_FuncionarioServiceConsultarFuncionario.py
@@ -2,20 +2,20 @@ import unittest
 from services import FuncionarioService
 from unittest.mock import MagicMock
 
+lista = []
+FuncionarioService.FuncionarioService.funcionarios = lista
+
 
 class TestFuncionarioServiceConsultar(unittest.TestCase):
 
     def setUp(self):
         # Configuração inicial para os testes
-        # Configuração inicial para os testes
         funcionario_mock1 = MagicMock(senha="1234", cpf="123456789", email="arthur.andre@gmail.com",
                                       documento="cpf", funcao="analista de segurança", idade=21, nome="Nome1", id=1)
         funcionario_mock2 = MagicMock(senha="1234567689", cpf="1234567892343", email="tutu.andre@gmail.com",
                                       documento="rg", funcao="analista de T.I.", idade=25, nome="Nome5", id=2)
-        FuncionarioService.FuncionarioService.funcionarios.append(
-            funcionario_mock1)
-        FuncionarioService.FuncionarioService.funcionarios.append(
-            funcionario_mock2)
+        lista.append(funcionario_mock1)
+        lista.append(funcionario_mock2)
 
     def test_consultarFuncionario_existente(self):
         # Chame a função consultarFuncionario para um ID existente
